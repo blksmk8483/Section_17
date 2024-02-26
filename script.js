204558a61642c79caa8e10f20a1fe8d071bc9255
Brandon Knight <knightbrandon1984@gmail.com>
2024-02-26 13:24:36 -0600

    module patter

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ add('ham', 4);
 console.log('The CARTS array:\n', ...cart);
 
 // ==============================================
-// =================TOP LEVEL AWAIT==============
+// ================TOP LEVEL AWAIT===============
 // ==============================================
 // console.log('Start fetching');
 
@@ -43,3 +43,35 @@ const getLastPost = async function () {
 
 const lastPost2 = await getLastPost();
 console.log(lastPost2);
+
+// ==============================================
+// ==============TOP MODULE PATTERN==============
+// ==============================================
+
+const shoppingCart2 = (function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 237;
+  const totalQuantity = 23;
+
+  const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(`${quantity} ${product} added to the cart.`);
+  };
+
+  const orderStock = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(`${quantity} ${product} ordered from supplier.`);
+  };
+
+  return {
+    addToCart,
+    cart,
+    totalPrice,
+    totalQuantity,
+  };
+})();
+
+shoppingCart2.addToCart('apple', 4);
+shoppingCart2.addToCart('pizza', 2);
+console.log(shoppingCart2);
